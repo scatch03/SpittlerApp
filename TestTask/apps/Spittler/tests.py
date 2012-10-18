264c73e648c1e6a26697a7c39557585f1151d30e
Alex <grimmy_grim@mail.ru>
2012-10-18 21:27:33 +0300

    Added tests for custom spittle rendering tag

--- TestTask/apps/Spittler/tests.py
+++ TestTask/apps/Spittler/tests.py
@@ -3,7 +3,9 @@
     Spittler Application Tests
 """
 
+from django.template import Template, Context
 from django_webtest import WebTest
+from TestTask.apps.Spittler.models import Spittle
 
 
 class SpittlerTest(WebTest):
@@ -18,4 +20,25 @@ class SpittlerTest(WebTest):
         self.assertTemplateUsed(response, u'spittles.html')
         assert u'first spittle' in response
         assert u'second spittle' in response
+
+
+class TemplateTagsTestCase(WebTest):
+    fixtures = [u'spittlers.json']
+
+    def setUp(self):
+        self.spittles = Spittle.objects.all()
+
+    def testRenderSpittle(self):
+        """ Test custom spittle rendering tag functionality """
+
+        t = Template('{% load render_spittle %}{% render_spittle identity %}')
+
+        for spittle in self.spittles:
+            c = Context({"identity": spittle.identity})
+            value = t.render(c)
+
+            assert spittle.title in value
+            assert spittle.message in value
+
+
 
